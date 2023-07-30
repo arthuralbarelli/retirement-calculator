0756e26be359682fe262f9714635f12fb96b19af
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-07-30 16:56:02 -0300

    tests(RetCalcSpecs.scala): create instance of RetCalc

--- app/src/test/scala/retcalc/RetCalcSpec.scala
+++ app/src/test/scala/retcalc/RetCalcSpec.scala
@@ -1,21 +1,23 @@
 package retcalc
 
 import org.scalactic.{Equality, TolerantNumerics, TypeCheckedTripleEquals}
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class RetCalcSpec extends WordSpec with Matchers with TypedCheckedTripleEquals {
+class RetCalcSpec extends AnyWordSpec with Matchers with TypeCheckedTripleEquals {
 
   implicit val doubleEquality: Equality[Double] =
     TolerantNumerics.tolerantDoubleEquality(0.0001)
 
   "RetCalc.futureCapital" should {
     "calculate the amount of savings I will have in n months" in {
-      val actual = RetCalc.futureCapital(
-        interestRate = 0.04 / 12, nbOfMonths = 25 * 12,
+      val retCalc = new RetCalc()
+      val actual = retCalc.futureCapital(
+        interestRate = 0.04.toFloat / 12, nbOfMonths = 25 * 12,
         netIncome = 3000, currentExpenses = 2000,
         initialCapital = 10000)
       val expected = 541267.1990
-      actual should ===(expected)
+      actual should === (expected)
     }
   }
 }

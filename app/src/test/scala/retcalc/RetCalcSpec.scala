9dd00ccfa554d9df9d061270ecc1599da43803dd
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-08-07 20:30:19 -0300

    test: Fix simulatePlan test

--- app/src/test/scala/retcalc/RetCalcSpec.scala
+++ app/src/test/scala/retcalc/RetCalcSpec.scala
@@ -22,14 +22,16 @@ class RetCalcSpec extends AnyWordSpec with Matchers with TypeCheckedTripleEquals
   }
 
   "RetCalc.simulatePlan" should {
-    "calculate the capital at the retirement and the capital after death"
-    val (capitalAtRetirement, capitalAfterDeath) = 
-    RetCalc.simulatePlan(
-      interestRate = 0.04 / 12,
-      nbOfMonthSavings = 25 * 12, nbMonthsInRetirement = 40 * 12,
-      netIncome = 3000, currentExpenses = 2000,
-      initialCapital = 10000)
-    capitalAtRetirement should === (541267.1990)
-    capitalAfterDeath should === (309867.5316)
+    "calculate the capital at the retirement and the capital after death" in {
+      val retCalc = new RetCalc()
+      val (capitalAtRetirement, capitalAfterDeath) = 
+      retCalc.simulatePlan(
+        interestRate = 0.04 / 12,
+        nbOfMonthSavings = 25 * 12, nbMonthsInRetirement = 40 * 12,
+        netIncome = 3000, currentExpenses = 2000,
+        initialCapital = 10000)
+      capitalAtRetirement should === (541267.1990)
+      capitalAfterDeath should === (309867.5316)
+    }
   }
 }
